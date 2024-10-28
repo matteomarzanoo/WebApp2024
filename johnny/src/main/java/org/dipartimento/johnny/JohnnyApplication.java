3c0ac27633df743d1edab37297483f7408bd177b
Matteo Marzano <mrzmtt03e31g273o@studenti.unical.it>
2024-10-28 17:55:22 +0100

    Initial commit

---
+++ johnny/src/main/java/org/dipartimento/johnny/JohnnyApplication.java
@@ -0,0 +1,13 @@
+package org.dipartimento.johnny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnnyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JohnnyApplication.class, args);
+    }
+
+}
---
+++ johnny/src/test/java/org/dipartimento/johnny/JohnnyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dipartimento.johnny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnnyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
